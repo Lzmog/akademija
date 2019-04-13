f6fa6a0fef06b674d52e0119369689abf43fb9fc
Lukas <lukas.zmoginas@gmail.com>
2019-04-14 01:01:18 +0300

    First react homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,125 @@
 import React from 'react';
 import Card from './Card';
 import axios from 'axios';
-import { endpoints, getImageUrl } from '../../config';
+import {endpoints, getImageUrl} from '../../config';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      movieList: [],
+    constructor() {
+        super();
+
+        this.state = {
+            genreList: [],
+            movieList: [],
+            currentGenre: null,
+        };
+
+        this.getMovies();
+        this.getGenreList();
+    }
+
+    getMovies = () => {
+        axios
+            .get(endpoints.mostPopularMovies())
+            .then((res) => this.setMovieList(res.data.results))
+            .catch((error) => console.log(error));
     };
-    
-    this.getMovies();
-  }
-  
-  getMovies = () => {
-    axios
-      .get(endpoints.mostPopularMovies())
-      .then((res) => this.setMovieList(res.data.results))
-      .catch((error) => console.log(error));
-  };
-  
-  setMovieList = (list) => {
-    this.setState({
-      movieList: list,
-    });
-  };
-  
-  render() {
-    const { movieList } = this.state;
-    
-    return (
-      <div>
-        {movieList.map((listItem) => (
-          <Card
-            backgroundImage={getImageUrl(listItem.backdrop_path)}
-            title={listItem.original_title}
-            releaseDate={listItem.release_date}
-            score={listItem.vote_average}
-            votes={listItem.vote_count}
-            description={listItem.overview}
-          />
-        ))}
-      </div>
-    );
-  }
+
+    setMovieList = (list) => {
+        const favoriteList = list.map((listItem) => {
+            return {...listItem, favorite: false}
+        });
+
+        this.setState({
+            movieList: favoriteList,
+        });
+    };
+
+    getGenreList = () => {
+        axios
+            .get(endpoints.genres())
+            .then((res) => this.setGenreList(res.data.genres))
+            .catch((error) => console.log(error));
+    };
+
+    setGenreList = (list) => {
+        this.setState({
+            genreList: list,
+        });
+    };
+
+    getCurrentGenre = (id) => {
+        this.setState({
+            currentGenre: id,
+        });
+    };
+
+    setDefaultGenreState = () => {
+        this.setState({
+            currentGenre: null,
+        });
+    };
+
+    setFavoriteMovie = (id) => {
+        const newList = this.state.movieList.map((listItem) => {
+            if (listItem.id === id) {
+                return {...listItem, favorite: !listItem.favorite}
+            } else {
+                return listItem
+            }
+        });
+
+        this.setState({movieList: newList})
+    };
+
+    render() {
+        const {movieList, genreList, currentGenre} = this.state;
+        const filteredMovies = currentGenre !== null ? movieList.filter(movie => {
+            if (movie.genre_ids.includes(currentGenre)) {
+                return movie;
+            }
+        }).map((listItem) => (
+            <Card
+                movieId={listItem.id}
+                backgroundImage={getImageUrl(listItem.backdrop_path)}
+                title={listItem.original_title}
+                releaseDate={listItem.release_date}
+                score={listItem.vote_average}
+                votes={listItem.vote_count}
+                description={listItem.overview}
+                setFavoriteMovie={this.setFavoriteMovie}
+                favorite={listItem.favorite}
+            />
+        )) : movieList.map((listItem) => (
+            <Card
+                movieId={listItem.id}
+                backgroundImage={getImageUrl(listItem.backdrop_path)}
+                title={listItem.original_title}
+                releaseDate={listItem.release_date}
+                score={listItem.vote_average}
+                votes={listItem.vote_count}
+                description={listItem.overview}
+                setFavoriteMovie={this.setFavoriteMovie}
+                favorite={listItem.favorite}
+            />
+        ));
+
+        return (
+            <div>
+                {genreList.map((listItem) => (
+                    <span
+                        className='genre'
+                        onClick={() => this.getCurrentGenre(listItem.id)}
+                    >
+              {listItem.name}
+            </span>
+                ))}
+                <span className='genre' onClick={() => this.setDefaultGenreState()}>
+                    All genres
+                </span>
+                {filteredMovies}
+            </div>
+        );
+    }
 }
 
 export default App;
